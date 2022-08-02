04b1b958c9e63171607bbd8132011bc87c80cf0f
omar <omarabomattr@gmail.com>
2022-08-02 16:18:27 +0300

    Initial Commit

---
+++ firstSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  firstSwiftUI
+//
+//  Created by OmarMattr on 02/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ firstSwiftUI/firstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstSwiftUIApp.swift
+//  firstSwiftUI
+//
+//  Created by OmarMattr on 02/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct firstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
